4e57b103d18ad1093f07fc1855bd81c69c749b2a
Omar Tammam <zft5ra2010@gmail.com>
2018-10-05 09:53:03 +0200

    Moving The Player
    + ignored .vs folder for not working

---
+++ Space Shooter/Assets/Space shooter assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // The speed at which our vehicle is moving per meter
+    public float Speed = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //current pos = new position
+        transform.position = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.Translate(Vector3.right * Time.deltaTime * Speed);
+	}
+}
